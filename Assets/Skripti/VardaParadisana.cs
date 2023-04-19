7e41d1e476a83d553017d49474b48f5d273a10d8
Gabriels Rosvalds <gabriels.rosvalds@gmail.com>
2023-04-19 12:20:27 +0300

    Updated projects

---
+++ Assets/Skripti/VardaParadisana.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VardaParadisana : MonoBehaviour {
+	public int kadruSk = 0;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Usākta programma! Tu esi " + kadruSk + "kadrā");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Debug.Log("Kards: " + kadruSk);
+		kadruSk++;
+	}
+}
